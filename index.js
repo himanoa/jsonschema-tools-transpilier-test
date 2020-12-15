23837f2ee8abab55113105236b04313face4fd0b
himanoa <matsunoappy@gmail.com>
2020-12-15 19:55:51 +0900

    TURAMI

--- index.js
+++ index.js
@@ -1,14 +1,20 @@
 const JsonSchemaTranspiler = require("@json-schema-tools/transpiler").default
-const referenceResolver = require("@json-schema-tools/reference-resolver").default;
-const { defineSchema } = require("@japan-d2/schema")
+const JsonSchemaDereferencer = require("@json-schema-tools/dereferencer").default;
+const { defineSchema, field } = require("@japan-d2/schema")
+
+const UserKind = field.enum('kind', ['readonly', 'normal', 'admin'])
 
 const user = defineSchema()
   .string('name', {maxLength: 255, minLength: 1})
   .integer("age", { minimum: 0 })
-  .enum('kind', ['readonly', 'normal', 'admin'])
+  .enum('kind', UserKind)
 
-const userSchema = {...user.toJSONSchema(), title: 'user'}
-const transpiler = new JsonSchemaTranspiler(referenceResolver(userSchema))
+async function run() {
+  const userSchema = user.toJSONSchema()
+  const dereferencer = new JsonSchemaDereferencer(userSchema)
+  const dereferencedSchema = await dereferencer.resolve()
+  const transpiler = new JsonSchemaTranspiler(dereferencedSchema)
+  console.log(transpiler.toTypescript())
+}
 
-console.log(user.toJSONSchema())
-console.log(transpiler.toTypescript())
+run()
