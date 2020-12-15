1531c8ca8e4759b77e128024d104c16e8a9df576
himanoa <matsunoappy@gmail.com>
2020-12-15 19:40:25 +0900

    Use @json-schema-tools/reference-resolver

--- index.js
+++ index.js
@@ -1,10 +1,14 @@
 const JsonSchemaTranspiler = require("@json-schema-tools/transpiler").default
-const { readFileSync } = require("fs")
+const referenceResolver = require("@json-schema-tools/reference-resolver").default;
+const { defineSchema } = require("@japan-d2/schema")
 
-const schema = JSON.parse(readFileSync("./qiita.json").toString())
+const user = defineSchema()
+  .string('name', {maxLength: 255, minLength: 1})
+  .integer("age", { minimum: 0 })
+  .enum('kind', ['readonly', 'normal', 'admin'])
 
-console.log(JSON.stringify(schema))
+const userSchema = {...user.toJSONSchema(), title: 'user'}
+const transpiler = new JsonSchemaTranspiler(referenceResolver(userSchema))
 
-const transpiler = new JsonSchemaTranspiler(schema)
-
+console.log(user.toJSONSchema())
 console.log(transpiler.toTypescript())
